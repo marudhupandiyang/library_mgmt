0cc1b1e67ebbb39992b1e361f2d76685c3d0164e
Marudhu Pandiyan G <marudhupandiyang@gmail.com>
2013-01-08 23:13:14 +0530

    Modified Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,63 +6,60 @@
 # #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 # #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# #seed books model
-# book = Book.create([{:title => 'Harry Potter' , :page => 250 , :count => 10 ,:price => 100},
-# 					{:title => 'Basic C' , :page => 150 , :count => 10 ,:price => 30},
-# 					{:title => 'Basic C++' , :page => 170 , :count => 10 ,:price => 50}
-# 					] )
+
+user = User.create([{:name => "admin" , :limit => 1, :regno => '123456' ,:admin => true}])
+
+user = User.create([{:name => "Marudhu" , :limit => 4, :regno => '08MS134'}])
 
 
+book = Book.create([{:title => 'Harry Potter' , :page => 250 , :count => 10 ,:price => 100}, {:title => 'Basic C' , :page => 150 , :count => 10 ,:price => 30}, {:title => 'Basic C plus plus' , :page => 170 , :count => 10 ,:price => 50}	])
 
-# #add a category for story books
-# book = Book.find_by_title('Harry Potter')
+#add a category for story books
+book = Book.find_by_title('Harry Potter')
 
-# category = book.categories.new
-# category.name = 'Stories'
-# category.save
+puts 'Inspecting Found book'
+puts book.inspect
 
-# tag = book.tags.new
-# tag.name= 'HarryPotter'
-# tag.save
+category = book.categories.new
+category.name = 'Stories'
 
-# book.categories.build
-# book.tags.build
-# book.save
+tag = book.tags.new
+tag.name= 'HarryPotter'
 
-
-# #add categories ,tags for Programming
-# book = Book.find_by_title('Basic C')
-
-# category = book.categories.new
-# category.name = 'Programming'
-# category.save
-
-# tag = book.tags.new
-# tag.name = 'C'
-# tag.save
-
-# book.categories.build
-# book.tags.build
-# book.save
+book.categories.build
+book.tags.build
+book.save
 
 
-# #add categories ,tags for Programming
-# book = Book.find_by_title('Basic C++')
+#add categories ,tags for Programming
+book = Book.find_by_title('Basic C')
 
-# category = book.categories.new
-# category.name = 'Programming'
-# category.save
+category = book.categories.new
+category.name = 'Programming'
 
-# tag = book.tags.new
-# tag.name = 'C++'
-# tag.save
+tag = book.tags.new
+tag.name = 'C'
 
-# book.categories.build
-# book.tags.build
-# book.save
+book.categories.build
+book.tags.build
+book.save
 
 
+#add categories ,tags for Programming
+book = Book.find_by_title('Basic C plus plus')
+
+category = book.categories.new
+category.name = 'Programming'
+
+tag = book.tags.new
+tag.name = 'C plus plus'
 
 
-# user = User.create([{:name => "Marudhu" , :limit => 4, :regno => '08MS134'}],
-# 					[{:name => "Pandiyan" , :limit => 4, :regno => '08MS135'}])
+book.categories.build
+book.tags.build
+book.save
+
+puts 'Admin adds 10 more books '
+book = Book.find_by_title('Harry Potter')
+book.add_book(10)
+
